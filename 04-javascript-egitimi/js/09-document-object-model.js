03c230a92e701c354ef76cda0ad733d9883d3514
Hakan Yalcinkaya <hakanyalcinkaya@gmail.com>
2020-12-17 22:48:59 +0300

    09-document-object-model

---
+++ 04-javascript-egitimi/js/09-document-object-model.js
@@ -0,0 +1,20 @@
+// DOM
+// https://developer.mozilla.org/en-US/docs/Web/API/Document_Object_Model
+
+// pathname
+console.log(document.location.pathname)
+
+// head icindeki bilgiler
+console.log(document.head)
+
+// body icindeki bilgiler
+console.log(document.body)
+
+// url bilgisi
+console.log(document.URL)
+console.log(document.baseURI)
+
+
+// document icerisinde style bilgilerini degistirmek:
+// css bilginizle birlikte style yapisi uzerinde duzenlemeler yapabilirsiniz..
+document.body.style.backgroundColor = "aqua"
