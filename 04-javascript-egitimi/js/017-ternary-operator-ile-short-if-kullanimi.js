a09e98e77c9c2b11fadfb017b9254b0ff56fbdc9
Hakan Yalcinkaya <hakanyalcinkaya@gmail.com>
2020-12-18 00:53:20 +0300

    017-ternary-operator-ile-short-if-kullanimi

---
+++ 04-javascript-egitimi/js/017-ternary-operator-ile-short-if-kullanimi.js
@@ -0,0 +1,13 @@
+// ********** ternary operator ile short if kullanimi **********
+
+// eger kullanici adin varsa yazdir yoksa kullanici bilginiz bulunamadi yaz
+
+let userName = prompt("Kullanici Bilginiz Yaziniz")
+let info = document.querySelector("#info")
+
+// ternary kullanimi:
+// kosul ? dogruysa : yanlissa
+
+// userName.length > 0 ? userName : "Kullani Bilginiz Bulunamadi :("
+
+info.innerHTML = `${userName ? userName : "Kullani Bilginiz Bulunamadi :("}`
