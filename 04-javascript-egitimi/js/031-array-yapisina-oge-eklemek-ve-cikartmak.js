84b9a03caf48379af1e908c5f7fcb406ba700902
Hakan Yalcinkaya <hakanyalcinkaya@gmail.com>
2020-12-18 20:00:17 +0300

    031-array-yapisina-oge-eklemek-ve-cikartmak

---
+++ 04-javascript-egitimi/js/031-array-yapisina-oge-eklemek-ve-cikartmak.js
@@ -0,0 +1,21 @@
+// Array yapisina oge eklemek ve Array icerisinden istenilen ogenin cikarilmasi
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array
+
+let items = [10, 20, 30, 40, ]
+console.log("items - ilk hali : ", items)
+
+// Array: sona oge/eleman eklemek -> push
+items.push(50)
+console.log("50 : ", items)
+
+// Array: basa oge eklemek -> unshift 
+items.unshift(5)
+console.log("5 : ", items)
+
+// Array: sondaki ogeyi cikarmak -> pop
+let lastItem = items.pop() // son elemani lastItem icerisine ekledik
+console.log("lastItem: ", lastItem, ", items: ", items)
+
+// Array: bastaki ogeyi cikarmak -> shift
+let firstItem = items.shift() // ilk elemani firstItem icerisine ekledik
+console.log("firstItem: ", firstItem, ", items: ", items)
