3da6fbcfdb2bdec15617417e5da7f27f6e02ab64
Hakan Yalcinkaya <hakanyalcinkaya@gmail.com>
2020-12-18 15:28:17 +0300

    024-localStorage-icerisine-veri-eklemek-duzenlemek-ve-silmek

---
+++ 04-javascript-egitimi/js/024-localStorage-icerisine-veri-eklemek-duzenlemek-ve-silmek.js
@@ -0,0 +1,15 @@
+// Local Storage:
+// https://developer.mozilla.org/en-US/docs/Web/API/Window/localStorage
+
+
+// localStorage icerisine bilgi kaydetmek:
+let email = "hakanyalcinkaya@gmail.com"
+localStorage.setItem('userEmail', email) // -> anahtar, deger yapisi kullanilir(key, value)
+
+// localStorage icerisindeki bilgiyi bir degiskene atamak ve cagirmak:
+let localStorageEMailInfo = localStorage.getItem('userEmail') // daha once atadigimiz anahtar bilgisi
+
+console.log(localStorageEMailInfo)
+
+// localStorage icerisinden istenilen Key bilgisinin silinmesi:
+localStorage.removeItem('userEmail') // key bilgisini yazarak silebiliriz..
