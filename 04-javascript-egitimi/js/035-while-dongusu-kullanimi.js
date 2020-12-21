ec8d4b757a0d9fd1751bb823d73edaf7ce004131
Hakan Yalcinkaya <hakanyalcinkaya@gmail.com>
2020-12-21 21:28:02 +0300

    035-while-dongusu-kullanimi

---
+++ 04-javascript-egitimi/js/035-while-dongusu-kullanimi.js
@@ -0,0 +1,16 @@
+// While Dongusu
+// https://www.w3schools.com/js/js_loop_while.asp
+
+// let counter = 0;
+
+// while ( counter < 10 ) {
+//   console.log(counter)
+//   counter ++
+// }
+
+let userName = ""
+
+while (!userName) {
+  userName = prompt("Kullanici Adini Giriniz: ")
+  console.log(userName)
+}
