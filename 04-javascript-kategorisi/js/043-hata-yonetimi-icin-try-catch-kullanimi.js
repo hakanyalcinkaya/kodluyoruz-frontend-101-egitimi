6ab73ddc801d1d2b9c522b4bbbfceab50017ff14
Hakan Yalcinkaya <hakanyalcinkaya@gmail.com>
2020-12-25 17:39:02 +0300

    043-hata-yonetimi

--- 04-javascript-kategorisi/js/043-hata-yonetimi-icin-try-catch-kullanimi.js
+++ 04-javascript-kategorisi/js/043-hata-yonetimi-icin-try-catch-kullanimi.js
@@ -5,8 +5,8 @@ try {
   products.forEach(item => console.log(item));
 } catch (error) {
   console.log("error:", error)
-  products = [1, 2, 3]
-  products.forEach(item => console.log(item));
+  // products = [1, 2, 3]
+  // products.forEach(item => console.log(item));
 }
 
 
